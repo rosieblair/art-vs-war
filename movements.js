734972f3c82aedef94f8d800f7651ffa97b53db2
christine-george <christine.george@gatech.edu>
2020-04-19 16:26:04 -0400

    incorporated beau's changes into my code, added popup function when clicking on art movement bars

--- movements.js
+++ movements.js
@@ -3,9 +3,18 @@ function dataPreprocessor(row) {
     return {
         movement: row.movement,
         start_year: +row.start_year,
-        end_year: +row.end_year ,
-        group: +row.group
+        end_year: +row.end_year,
+        group: +row.group,
+        description: row.description
     };
+}
+
+function metDataPreprocessor(row) {
+	// met data row fields that we want? Perhaps I may be missing some
+	return {
+		Object_ID: +row.Object_ID,
+
+	};
 }
 
 var svg = d3.select('svg');
@@ -39,18 +48,44 @@ var colors = [
             ['#A0D568', '21 - 50 years'],
             ['#4FC1E8', '51 - 100 years'],
             ['#AC92EB', '100+ years']
-        ];
+		];
 
-d3.csv('movements.csv', dataPreprocessor).then(function(dataset) {
-	console.log(dataset);
+//function to chose movement bar color based on movement duration
+function barColorFill(start_year, end_year) {
+    if ((end_year - start_year) <= 10) {
+        return colors[0][0];
+    } else if (((end_year - start_year) >= 11)
+        && ((end_year - start_year) <= 20)) {
+        return colors[1][0];
+    } else if (((end_year - start_year) >= 21)
+        && ((end_year - start_year) <= 50)) {
+        return colors[2][0];
+    } else if (((end_year - start_year) >= 51)
+        && ((end_year - start_year) <= 100)) {
+        return colors[3][0];
+    } else {
+        return colors[4][0];
+    }
+}
+
+Promise.all([
+	d3.csv('movements.csv', dataPreprocessor),
+	d3.csv('MetObjects0.csv')
+]).then(function(dataset) {
+	console.log("Data has been loaded in");
+
+	movementData = dataset[0];
+	metData = dataset[1];
+
+/*            --------- Movement Visualisation start -----------          */
 
 	//returns the earliest year in the dataset
-	var yearMin = d3.min(dataset, function(d) {
+	var yearMin = d3.min(movementData, function(d) {
     	return +d['start_year'];
     });
 
 	//returns the most recent year in the dataset
-    var yearMax = d3.max(dataset, function(d) {
+    var yearMax = d3.max(movementData, function(d) {
     	return +d['end_year'];
     });
 
@@ -67,53 +102,15 @@ d3.csv('movements.csv', dataPreprocessor).then(function(dataset) {
         .attr('transform', 'translate('+[padding.l, svgHeight - padding.b]+')')
         .call(timeline);
 
-    //appends all bars to the timeline
-	svg.append('g')
-		.selectAll('rect')
-		.data(dataset)
-		.enter()
-		.append('rect')
-		.attr('class', 'bar')
-		.attr('x', function(d) {return (timelineRange(d.start_year) + padding.l);})
-		.attr('y', function(d) {return (barHeightScale(d.group) + padding.t - 35);})
-		.attr('rx', 5)
-		.attr('ry', 5)
-		.attr('width', function(d) {return ((timelineRange(d.end_year)) - (timelineRange(d.start_year)));})
-		.attr('height', barHeight)
-		.style('fill', function(d) { //fill bar color based on movement duration
-            if ((d.end_year - d.start_year) <= 10) {
-                return colors[0][0];
-            } else if (((d.end_year - d.start_year) >= 11)
-                && ((d.end_year - d.start_year) <= 20)) {
-                return colors[1][0];
-            } else if (((d.end_year - d.start_year) >= 21)
-                && ((d.end_year - d.start_year) <= 50)) {
-                return colors[2][0];
-            } else if (((d.end_year - d.start_year) >= 51)
-                && ((d.end_year - d.start_year) <= 100)) {
-                return colors[3][0];
-            } else {
-                return colors[4][0];
-            }
-        });
-
-	//appends movement names to each bar
-	svg.append('g')
-		.selectAll('bar')
-		.data(dataset)
-		.enter()
-		.append('text')
-		.text(function(d) {return d.movement;})
-		.attr('x', function(d) {return (timelineRange(d.start_year) + padding.l);})
-		.attr('y', function(d) {return (barHeightScale(d.group) + padding.t - 35);})
-		.style('opacity', 0); //currently set to 0 to not show labels, trying to figure out how to show
-		//them only when you over over the bar
+    //updates chart after filters are applied
+    updateChart('all-movements')
 
     //adding color legend title to the right side
     svg.append("text")
         .attr("x", (svgWidth - (padding.r * 0.9)))
         .attr("y", (svgHeight * 0.35))
         .style("font-size", "20px")
+        .style("font-weight", 600)
         .text("Movement Length")
 
     //appending color legend circles
@@ -162,6 +159,7 @@ d3.csv('movements.csv', dataPreprocessor).then(function(dataset) {
         .attr("x", (padding.l))
         .attr("y", (padding.t / 2))
         .style("font-size", "30px")
+        .style("font-weight", 600)
         .text("The Art of War")
 
     //adding description to the top
@@ -169,6 +167,90 @@ d3.csv('movements.csv', dataPreprocessor).then(function(dataset) {
         .attr("x", (svgWidth * 0.56))
         .attr("y", (padding.t / 2))
         .style("font-size", "20px")
-        .text("A timeline of art movements across periods of war.")
+		.text("A timeline of art movements across periods of war.")
 
-	});
+    //create popups for movements and years
+    var tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("z-index", 10)
+        .style("opacity", 0);
+
+    //updates chart with filter changes
+    function updateChart(filterkey) {
+        console.log('Called updateChart');
+        var filteredMovements = movementData;
+        console.log(filteredMovements);
+        var bars = chartG.selectAll('.bar')
+            .data(filteredMovements);
+
+        var barsEnter = bars.enter()
+            .append('g')
+            .attr('class', 'bar')
+            .on('mouseover', function(d) {
+                // Use this to select the hovered element
+                var hovered = d3.select(this);
+                // add hovered class to style the group
+                hovered.classed('hovered', true);
+                // add a new text value element to the group
+                hovered.append('text')
+                    .attr('class', 'value')
+                    .text(d.movement)
+                    .attr('x', function(d) {
+                        return (timelineRange(d.start_year));
+                    })
+                    .attr('y', function(d) {
+                        return (barHeightScale(d.group) - 40);
+                    });
+                hovered.raise();
+            })
+            .on('mouseout', function(d) {
+                // Clean up the actions that happened in mouseover
+                var hovered = d3.select(this);
+                hovered.classed('hovered', false)
+                hovered.select('text.value').remove();
+            })
+            .on('click', function(d) {
+                //shows information popup when you click on the movement bar
+                 tooltip.transition().duration(150)
+                    .text(d.movement + " (" + d.start_year + " - "
+                    + d.end_year + "): " + d.description)
+                    .style("top", (d3.event.pageY) + "px")
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("opacity", 0.8);
+
+                d3.event.stopPropagation();
+            });
+
+        //listener to reset popup when you click off of the movement bar
+        d3.select('body').on('click', resetTooltip);
+
+        //hides popup
+        function resetTooltip() {
+            tooltip.transition().duration(150)
+                .style("opacity", 0);
+        }
+
+        bars.merge(barsEnter);
+
+        //append movement bars to timeline
+        barsEnter.append('rect')
+            .attr('rx', 5)
+            .attr('ry', 5)
+            .attr('height', barHeight)
+            .attr('width', function(d) {return ((timelineRange(d.end_year)) - (timelineRange(d.start_year)));})
+            .attr('x', function(d) {return (timelineRange(d.start_year));})
+            .attr('y', function(d) {return (barHeightScale(d.group) - 40);})
+            //fill bar color based on movement duration
+            .style('fill', function(d) {return (barColorFill(d.start_year, d.end_year));});
+
+    }
+
+/*            --------- Movement Visualisation End -----------          */
+
+/*            --------- MetData Visualisation Start -----------          */
+
+/*            --------- MetData Visualisation end -----------          */
+
+});
